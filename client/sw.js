3f688deb90fe441dafe6bb1e671b220748b9f4ae
Muzaffarkhon <muzaffarkhonr@gmail.com>
2026-09-11 15:09:53 +0500

    docs: сводный пакет передачи контекста и прогресса за 11.09.2026

--- client/sw.js
+++ client/sw.js
@@ -1,5 +1,5 @@
 // Farovon Market Analysis PWA Service Worker
-const CACHE_NAME = 'farovon-market-v2-5-7';
+const CACHE_NAME = 'farovon-market-v2-5-8';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
